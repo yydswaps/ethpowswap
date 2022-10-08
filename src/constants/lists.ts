97221147c3e9e36922f8c33584839848cf2696fe
yydswaps <ringsfrog19@gmail.com>
2022-10-08 14:50:54 +0800

    first commit

---
+++ src/constants/lists.ts
@@ -0,0 +1,8 @@
+// the ethpowswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/yydswaps/ethswaptokenlist/main/ethpowswap.tokenlist.json'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL,
+  'https://gateway.pinata.cloud/ipfs/QmaXGLRyVP2PEbxrB2gfjoKKVfxX3Pk7gYyPaLSvXXqrFF',
+  'https://raw.githubusercontent.com/yydswaps/ethswaptokenlist/main/ethpowswap.tokenlist.json'
+]
